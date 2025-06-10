30d92bd0e6d0acd0b1286b21abd21ece546b4752
unknown <wajihurrehman2001@gmail.com>
2025-06-10 21:37:16 +0500

    trying things out

---
+++ basics.py
@@ -0,0 +1,9 @@
+cars=["civic","accord","corolla","camry"]
+bikes=["honda","yamaha","suzuki","kawasaki"]
+
+cars.append(bikes[1])
+new =cars
+
+ayo=[1,1,1,1,1,1,1,2,2,2,2,2,2,2,3,3,3,3,3,3,]
+x=set(ayo)
+print(x)
