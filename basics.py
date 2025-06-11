5365afdae79f05ef671b7aa35264935074fdd97e
unknown <wajihurrehman2001@gmail.com>
2025-06-11 16:37:07 +0500

    variables excercise on https://pythonbasics.org/variables/

--- basics.py
+++ basics.py
@@ -1,10 +1,4 @@
-cars=["civic","accord","corolla","camry"]
-bikes=["honda","yamaha","suzuki","kawasaki"]
-
-cars.append(bikes[1])
-
-
-sett=[1,1,1,1,1,1,1,2,2,2,2,2,2,2,3,3,3,3,3,3,]
-x=set(sett)
-print(x)
-print(cars)
+x=65
+y=24
+sum=x+y
+print("the sum of ",x ,"and", y, "is",sum)
